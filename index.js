e922213dc8087d0d7d3514f0b456622a9868c0d7
RISHIKA SINHA <142867508+rishikasinha29@users.noreply.github.com>
2024-08-11 22:50:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var inputvalue = document.querySelector('#cityinput')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+apik 
